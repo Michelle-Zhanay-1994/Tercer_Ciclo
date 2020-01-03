5eb033456f5aebb5f6e7d88a3bee7f4e322cc04e
Michelle <51102312+Michelle-Zhanay-1994@users.noreply.github.com>
2020-01-03 09:46:41 -0500

    Add files via upload

---
+++ cadena.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Joption;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class cadena {
+
+    public static void main(String[] args) {
+        char caracteres = 0;
+        String cadena;
+        String correo = "";
+        char arroba = '@';
+        int numero = 0;
+        String mensaje;
+
+        cadena = JOptionPane.showInputDialog("INGRESE SU CORREO ELECTRONICO");
+
+        for (int i = 0; i < cadena.length(); i++) {
+
+            caracteres = cadena.charAt(i);
+
+            correo = correo + caracteres;
+
+            if (cadena.charAt(i) == arroba) {
+
+                numero++;
+
+            }
+
+        }
+
+        JOptionPane.showMessageDialog(null, numero);
+    }
+}
